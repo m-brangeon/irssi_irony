00d8276e0e1a5e760f4ef6c6e5991d467b5b0f46
Mickael Brangeon <mickael.brangeon@infomaniak.com>
2017-06-14 15:05:32 +0200

    add cool story bro tag

--- irssi_irony.pl
+++ irssi_irony.pl
@@ -8,7 +8,7 @@ $VERSION="0.0.1";
 	authors	=> 'Pierre-Matthieu Alamy',
 	contact	=> 'pm+irssi@alamy.fr',
 	name	=> 'irony',
-	description	=> 'Insert <irony> tags in setences',
+	description	=> 'Insert <irony> tags in setences and <cool story bro>',
 	license	=> 'BSD 3-clause',
 	url	=> 'https://github.com/GrumpyCorp/irssi_irony',
 );
@@ -27,5 +27,15 @@ sub cmd_irony {
 		$witem->command("/SAY <irony>$data</irony>");
 	}
 }
+sub cmd_coolstory {
+	my ($data, $server, $witem) = @_;
+	if (!$server || !$server->{connected}) {
+		Irssi::print("Not connected to server");
+		return;
+	}
+	if ($data) {
+		$witem->command("/SAY <cool story bro>$data</cool story bro>");
+}
 
 Irssi::command_bind('irony', 'cmd_irony');
+Irssi::command_bind('coolstory', 'cmd_coolstory');
